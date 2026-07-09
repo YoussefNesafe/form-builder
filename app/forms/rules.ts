ad14916a9446dcf4016e8d434ef970c5a587f715
YoussefNesafe <ynessafe@gmail.com>
2026-07-10 02:19:11 +0400

    remove: pages

--- app/forms/rules.ts
+++
@@ -1,9 +0,0 @@
-import type { TextRules } from "@/form-builder";
-
-export const nameRules: TextRules = {
-  minLength: 2,
-  pattern: "^[A-Za-z ]+$",
-  trim: true,
-  allow: "A-Za-z ",
-  message: "Only letters and spaces are allowed",
-};
